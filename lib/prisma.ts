e47b10f63765cf33cf23d5a0b88c6f75c172f77c
ztargazer57 <johnmhellsinagpulo@gmail.com>
2026-04-18 16:20:57 +0800

    Prisma Adapter Switched to Postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,17 @@
 import { PrismaClient } from "@/generated/prisma";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    connectionLimit: 5,
-})
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter,});
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
